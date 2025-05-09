266e8b5b0f27735d5d0865ed9c38972ac769cd74
A_S_MHD <mhmdnourzureik369@gmail.com>
2025-05-09 18:43:43 +0300

    finish App Configeration

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,21 @@
+/* --
+      LIST OF Enums
+      They cannot be created inside a class.
+-- */
+
+/// Switch of Custom Brand-Text-Size Widget
+enum AppTextSizes { small, medium, large }
+
+enum AppOrderStatus { processing, shipped, delivered }
+
+enum AppPaymentMethods {
+  paypal,
+  googlePay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  paystack,
+  razorPay,
+  paytm
+}
